c014100f9152cda1282315501275b026b1afa071
Shubham Tiwari <125534462+ShubhamTiwari55@users.noreply.github.com>
2025-06-23 15:44:52 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'http://16.16.208.117:5000',
 });
 
 API.interceptors.request.use((req) => {
